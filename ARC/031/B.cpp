bce10810a7ac19158ece0cc8c72c7d6965a634ba
Tigar <tigar@example.com>
2026-03-12 16:24:29 +0900

    B.cpp

--- ARC/031/B.cpp
+++ ARC/031/B.cpp
@@ -94,95 +94,95 @@
 #include <variant>
 #include <vector>
 #include <version>
-void input(int i, int j,int &whole_count, std::vector<std::vector<char>> &data) {
+int whole_count = 0, a = 0, b = 0, c = 0, d = 0;
+std::vector<std::vector<char>> data(10, std::vector<char>(10));
+std::vector<std::vector<bool>> first(10, std::vector<bool>(10, false));
+std::vector<std::vector<bool>> second(10, std::vector<bool>(10, false));
+void input(int i, int j) {
     if (i == 10) return;
     std::cin >> data[i][j];
     if (data[i][j] == 'o') whole_count++;
-    j == 9 ? input(i + 1, 0, whole_count, data) : input(i, j + 1, whole_count, data);
+    j == 9 ? input(i + 1, 0) : input(i, j + 1);
 }
-void first_cell (int i, int j, std::vector<std::vector<char>> &data, int &a, int &b) {
+void first_cell (int i, int j) {
     if (i == 10) return;
     if (data[i][j] == 'o') {
         a = i;
         b = j;
         return;
     }
-    j == 9 ? first_cell(i + 1, 0, data, a, b) : first_cell(i, j + 1, data, a, b);
+    j == 9 ? first_cell(i + 1, 0) : first_cell(i, j + 1);
 }
-void expand(int i, int j, int &count, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &checked) {
+void expand(int i, int j, int &count, std::vector<std::vector<bool>> &checked) {
     if (i == -1 || i == 10 || j == -1 || j == 10) return;
     if (checked[i][j]) return;
     if (data[i][j] == 'x') return;
     checked[i][j] = true;
     count++;
-    expand(i - 1, j, count, data, checked);
-    expand(i + 1, j, count, data, checked);
-    expand(i, j - 1, count, data, checked);
-    expand(i, j + 1, count, data, checked);
+    expand(i - 1, j, count, checked);
+    expand(i + 1, j, count, checked);
+    expand(i, j - 1, count, checked);
+    expand(i, j + 1, count, checked);
 }
-bool before_oneisland (int i, int j, int &whole_count, std::vector<std::vector<char>> &data) {
+bool before_oneisland (int i, int j) {
     int count = 0;
     std::vector<std::vector<bool>> checked(10, std::vector<bool>(10, false));
-    expand(i, j, count, data, checked);
+    expand(i, j, count, checked);
     return (count == whole_count) ? true : false;
 }
-void first_island (int i, int j, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &first) {
+void first_island (int i, int j) {
     int e = 0;
-    expand(i, j, e, data, first);
+    expand(i, j, e, first);
 }
-void second_cell (int i, int j, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &first, int &c, int &d) {
+void second_cell (int i, int j) {
     if (i == 10) return;
     if (data[i][j] == 'o' && first[i][j] == false) {
         c = i;
         d = j;
         return;
     }
-    j == 9 ? second_cell(i + 1, 0, data, first, c, d) : second_cell(i, j + 1, data, first, c, d);
+    j == 9 ? second_cell(i + 1, 0) : second_cell(i, j + 1);
 }
-void expand2(int i, int j, int &count, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &checked , std::vector<std::vector<bool>> &first) {
+void expand2(int i, int j, int &count, std::vector<std::vector<bool>> &checked) {
     if (i == -1 || i == 10 || j == -1 || j == 10) return;
     if (checked[i][j]) return;
     if (data[i][j] == 'x') return;
     if (first[i][j] == true) return;
     checked[i][j] = true;
     count++;
-    expand2(i - 1, j, count, data, checked, first);
-    expand2(i + 1, j, count, data, checked, first);
-    expand2(i, j - 1, count, data, checked, first);
-    expand2(i, j + 1, count, data, checked, first);
+    expand2(i - 1, j, count, checked);
+    expand2(i + 1, j, count, checked);
+    expand2(i, j - 1, count, checked);
+    expand2(i, j + 1, count, checked);
 }
-void second_island (int i, int j, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &first, std::vector<std::vector<bool>> &second) {
+void second_island (int i, int j) {
     int f = 0;
-    expand2(i, j, f, data, second, first);
+    expand2(i, j, f, second);
 } 
-bool after_oneisland (int i, int j, int &whole_count, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &first, std::vector<std::vector<bool>> &second) {
+bool after_oneisland (int i, int j) {
     if (i == 10) return false;
     if (data[i][j] == 'x' && ((i != 0 && first[i - 1][j] == true) || (i != 9 && first[i + 1][j] == true) || (j != 0 && first[i][j - 1] == true) || (j != 9 && first[i][j + 1] == true)) && ((i != 0 && second[i - 1][j] == true) || (i != 9 && second[i + 1][j] == true) || (j != 0 && second[i][j - 1] == true) || (j != 9 && second[i][j + 1] == true))) {
         data[i][j] = 'o';
         int count = 0;
         std::vector<std::vector<bool>> checked(10, std::vector<bool>(10, false));
-        expand(i, j, count, data, checked);
+        expand(i, j, count, checked);
         data[i][j] = 'x';
         if (count == whole_count + 1) return true;
     }
-    return j == 9 ? after_oneisland(i + 1, 0, whole_count, data, first, second) : after_oneisland(i, j + 1, whole_count, data, first, second);
+    return j == 9 ? after_oneisland(i + 1, 0) : after_oneisland(i, j + 1);
 }
-int whole_count = 0, a = 0, b = 0, c = 0, d = 0;
-std::vector<std::vector<char>> data(10, std::vector<char>(10));
-std::vector<std::vector<bool>> first(10, std::vector<bool>(10, false));
-std::vector<std::vector<bool>> second(10, std::vector<bool>(10, false));
-void combine (int &whole_count, int &a, int &b, int &c, int &d, std::vector<std::vector<char>> &data, std::vector<std::vector<bool>> &first, std::vector<std::vector<bool>> &second) {
+void combine () {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    input(0, 0, whole_count, data);
-    first_cell(0, 0, data, a, b);
-    if (before_oneisland(a, b, whole_count, data)) std::cout << "YES" << '\n';
+    input(0, 0);
+    first_cell(0, 0);
+    if (before_oneisland(a, b)) std::cout << "YES" << '\n';
     else {
-        first_island(a, b, data, first);
-        second_cell(0, 0, data, first, c, d);
-        second_island(c, d, data, first, second);
-        std::cout << (after_oneisland(0, 0, whole_count, data, first, second) ? "YES" : "NO") << '\n';
+        first_island(a, b);
+        second_cell(0, 0);
+        second_island(c, d);
+        std::cout << (after_oneisland(0, 0) ? "YES" : "NO") << '\n';
     }
 }
-int main() {combine(whole_count, a, b, c, d, data, first, second);}
+int main() {combine();}
 //埋め立て
